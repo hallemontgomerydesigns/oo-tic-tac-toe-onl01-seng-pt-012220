7d1bbfdd70597763efdb3b62d84f16ad6932e8dd
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-02-24 01:41:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,7 +92,7 @@ class TicTacToe
   def play
     
     turn until over?
-    puts winner? "Congratulations #{winner}!" : "Cat's Game!"
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
     
   end
   
